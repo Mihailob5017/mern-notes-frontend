861ddf8771f139006a37e0784c736068360cc468
Mihailo Boskovic <mihailob5017@gmail.com>
2019-12-12 19:08:31 +0100

    Updates

---
+++ precache-manifest.50bffbc0f20508145f4d45020ced0a0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd08439d30854f50304878eec65c5ce6",
+    "url": "/mern-notes-frontend/index.html"
+  },
+  {
+    "revision": "753da5b16a6c60825e38",
+    "url": "/mern-notes-frontend/static/css/2.e315d21c.chunk.css"
+  },
+  {
+    "revision": "b3ca799820dfab481775",
+    "url": "/mern-notes-frontend/static/css/main.58083abc.chunk.css"
+  },
+  {
+    "revision": "753da5b16a6c60825e38",
+    "url": "/mern-notes-frontend/static/js/2.944a922f.chunk.js"
+  },
+  {
+    "revision": "18b6a5bf156c218c69888d92513e7b9e",
+    "url": "/mern-notes-frontend/static/js/2.944a922f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3ca799820dfab481775",
+    "url": "/mern-notes-frontend/static/js/main.9d841bab.chunk.js"
+  },
+  {
+    "revision": "51286e807cb057ce636f",
+    "url": "/mern-notes-frontend/static/js/runtime-main.28d89433.js"
+  }
+]);
